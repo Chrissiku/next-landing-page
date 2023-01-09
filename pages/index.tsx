03c04b7b190b45fa8fdc4f64b4dbde65035d7a3c
Chris Siku <chrissiku5@gmail.com>
2023-01-09 22:52:17 +0200

    Implement `Feature` component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Feature from "../components/Feature";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
@@ -13,7 +14,7 @@ const Home: NextPage = () => {
       </Head>
       <Header />
       <Hero />
-
+      <Feature />
       <main>Main page content</main>
       <Footer />
     </div>
